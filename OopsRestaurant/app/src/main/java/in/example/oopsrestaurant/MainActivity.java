ca1753c2354266f715b58ca0cd057b07bc205bd1
amanjn38 <amanjn38@gmail.com>
2020-04-10 11:08:24 +0530

    IC

---
+++ OopsRestaurant/app/src/main/java/in/example/oopsrestaurant/MainActivity.java
@@ -0,0 +1,14 @@
+package in.example.oopsrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
